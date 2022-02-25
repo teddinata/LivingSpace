3e594d5ad6eb41ba9864ef87a4c357d0bff56c31
teddinata <teddinata@users.noreply.github.com>
2022-02-25 14:48:10 +0700

    Completed Blade and Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontendController::class, 'index'])->name('index');
+Route::get('/detail/{slug}', [FrontendController::class, 'detail'])->name('detail');
+Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
+Route::get('/checkout/success', [FrontendController::class, 'success'])->name('success');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function(){
+    Route::get('/', [DashboardController::class, 'index'])->name('index'); //dashboard/index
+
+    Route::middleware(['admin'])->group(function(){
+        //
+    });
+});
